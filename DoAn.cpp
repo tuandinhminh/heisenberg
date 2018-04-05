0a77cf042ceb706e065257edafc4a78b0fbf980f
Tuan Dinh Minh <33822487+tuandinhminh@users.noreply.github.com>
2018-04-05 17:44:39 +0700

    Do An

---
+++ DoAn.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+class Customer{
+	protected:
+		int MaKH;
+		char *Ten;
+		unsigned long cmt;
+		int NS[3];
+	public:
+		Customer();
+		
+};
