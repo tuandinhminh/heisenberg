58cb098e308ca1bbf1013f2cf7cebb689e51c4ec
Tuan Dinh Minh <33822487+tuandinhminh@users.noreply.github.com>
2018-04-06 14:38:19 +0700

    Update DoAn.cpp

--- DoAn.cpp
+++ DoAn.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 class Customer{
 	protected:
-		int MaKH;
+		int maKH;
 		char *Ten;
 		unsigned long cmt;
 		int NS[3];
@@ -11,4 +11,25 @@ class Customer{
 		Customer();
 		~Customer();
 		
+		
 };
+
+class Account:public Customer{
+	protected:
+		int maTK;
+		bool loaiTK=false;
+		long soDu;
+	public:
+		Account();
+		~Account();
+		void nopTien(long );
+		void rutTien(long );
+		void chuyenKhoan(long ,const Account &);
+		void congTienLai(double );
+};
+class Transaction:public Account{
+	
+};
+int main(){
+	return 0;
+}
