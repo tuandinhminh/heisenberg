54fa4a0fc889b80d127a7b24f8658b03c484ff7f
Tuan Dinh Minh <33822487+tuandinhminh@users.noreply.github.com>
2018-04-06 17:57:49 +0700

    Update DoAn.cpp

--- DoAn.cpp
+++ DoAn.cpp
@@ -1,44 +1,84 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 class Customer{
 	protected:
 		int maKH;
-		char *Ten;
-		unsigned long cmt;
-		int NS[3];
+		string Ten;
+		long cmt;
+		long NS;
 	public:
-		Customer();
-		~Customer();
+		Customer(){
+			maKH = 0;
+			NS=0;	
+			cmt=0;
+			Ten="";
+		};
+		Customer(string ,int ,long ,int);
+		string getTen(){
+			return Ten;
+		}
+		int getmaKH(){
+			return maKH;
+		}
 		void setmaKH(int );
-		void setTen(char* );
+		void setTen(string );
 		void setcmt(long);
 		void setNS(int ,int ,int);
 		
 };
-
+Customer::Customer(string Ten,int maKH,long cmt,int ngay){
+	this->maKH = maKH;
+	this->NS=ngay;	
+	
+	this->cmt=cmt;
+	this->Ten =Ten;
+		
+}
 class Account:public Customer{
 	protected:
+		
 		int maTK;
-		bool loaiTK=false;
+		string tenTK;
+		string loaiTK;
 		long soDu;
 	public:
 		Account();
-		~Account();
 		void nopTien(long );
 		void rutTien(long );
 		void chuyenKhoan(long ,const Account &);
 		void congTienLai(double );
 };
 class Transaction:public Account{
+		int maGD;
 		string loaiGD;
-		int soLan;
-		int ngayGD[3];
+		int ngayGD;
+		long soTien;
 	public:
 		Transaction();
-		~Transaction();
+		
 };
 int main(){
+	Customer C;
+	int ctr;
+	cout<<"---------------------------------"<<endl;
+	cout<<"1-Them khach hang"<<endl;
+	cout<<"2-Them tai khoan"<<endl;
+	cin>>ctr;
+	if (ctr == 1 ){
+		string c;
+		cout<<"ten :";
+		cin>>c;
+		
+		int t;
+		cout<< "moi nhap ma khach hang :";
+		fflush(stdin);
+		cin>>t;
+		
+		Customer C(c,t,3,4);
+		cout<<C.getmaKH()<<" "<<C.getTen();
+	}
 	
 	return 0;
 }
